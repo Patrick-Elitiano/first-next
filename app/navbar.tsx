ac937f38d17f8543512101b25db8a781ff55e340
Patrick Henrique Medeiros Dos Santos <121632881+Patrick-Elitiano@users.noreply.github.com>
2023-07-06 16:25:06 -0300

    add css

---
+++ app/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar(){
+    return (
+        <div className="text-4xl decoration-white	bg-gray-600		">
+            <h1>Paises Cadastrados</h1>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 import sql from "./db";
+import Navbar from "./navbar";
 
 export default async function Lista() {
   const paises = await sql`select id, nome, abbr from paises`;
 
-  console.log(paises);
-
   return (
-    <ul>
-      {paises.map((p) => (
-        <li key={p.id}>
-          {p.id} - {p.nome} ({p.abbr})
-        </li>
-      ))}
-    </ul>
+    <div className="h-screen bg-black">
+      <div className="flex justify-center	">  
+      </div>
+      <div>
+        <ul>
+          {paises.map((p) => (
+            <li key={p.id}>
+              {p.id} - {p.nome} ({p.abbr})
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
